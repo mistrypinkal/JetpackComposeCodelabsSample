658d15d7ef43d693d438efb8eca291baa2b67030
Pinkal Mistry <pinkald.mistry@gmail.com>
2022-06-19 11:41:09 +0400

    Codelab sample created Commit

---
+++ app/src/main/java/com/pm/jetpackcomposecodelabssample/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.pm.jetpackcomposecodelabssample.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Navy = Color(0xFF073042)
+val Blue = Color(0xFF4285F4)
+val LightBlue = Color(0xFFD7EFFE)
+val Chartreuse = Color(0xFFEFF7CF)
---
+++ app/src/main/java/com/pm/jetpackcomposecodelabssample/ui/theme/Theme.kt
@@ -0,0 +1,51 @@
+package com.pm.jetpackcomposecodelabssample.ui.theme
+
+import android.graphics.Color
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = Chartreuse,
+    primary = LightBlue,
+    onPrimary = Navy
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun JetpackComposeCodelabsSampleTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
